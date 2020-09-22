3305e61c91ebd6ee3b929766095ef5a7053d8803
Nikita Sheremet <nikita.sheremet253@gmail.com>
2020-09-21 18:14:27 -0700

    add file ending

--- build/main.js
+++ build/main.js
@@ -1,4 +1,4 @@
-import { createHeader } from "./second";
+import { createHeader } from "./second.js";
 var myName = createHeader("Nikita Sheremet");
 document.body.prepend(myName);
 console.log(myName);
