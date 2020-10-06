e4c54bdd623f035e5092ddbb96f43e126ee5dd3e
Nikita Sheremet <nikita.sheremet253@gmail.com>
2020-10-05 19:58:11 -0700

    add animation to name

--- build/main.js
+++
@@ -1,4 +0,0 @@
-import { createHeader } from "./second.js";
-var myName = createHeader("Nikita Sheremet");
-document.body.prepend(myName);
-console.log(myName);
---
+++ public/main.js
@@ -0,0 +1,4 @@
+// import { createHeader } from "./second.js";
+// var myName = createHeader("Nikita Sheremet");
+// document.body.prepend(myName);
+// console.log(myName);
